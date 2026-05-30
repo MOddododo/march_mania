49ddf9741ad4bc26bacf7ee0f6535c14ff0de75b
MOddododo <wenchizhou@petalmail.com>
2026-05-30 19:02:01 +0800

    test

---
+++ code/get_envir.py
@@ -0,0 +1,18 @@
+import os
+
+def is_kaggle() -> bool:
+    """判断当前是否在 Kaggle 云端运行"""
+    return "KAGGLE_URL_BASE" in os.environ or "KAGGLE_KERNEL_RUN_TYPE" in os.environ
+
+def get_github_token() -> str:
+    if is_kaggle():
+        from kaggle_secrets import UserSecretsClient
+        secret_label = "GH_TOKEN"
+        secret_value = UserSecretsClient().get_secret(secret_label)
+        return secret_value
+    else:
+        from dotenv import load_dotenv
+        load_dotenv(override=True)
+        secret_value = os.environ.get("GH_TOKEN", "").strip()
+        return secret_value
+    
